9b3f053d28665aa43b5db133ea83915b647f78c9
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-03 16:29:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 const firebaseConfig = { apiKey: "AIzaSyAzfEMwMd6M1VgvV0tJn7RS63RJghLE5UI", authDomain: "albergues-temporales.firebaseapp.com", projectId: "albergues-temporales", storageBucket: "albergues-temporales.firebasestorage.app", messagingSenderId: "489999184108", appId: "1:489999184108:web:32b9b580727f83158075c9" };
 const app = initializeApp(firebaseConfig); 
 const auth = getAuth(app); 
-const db = getFirestore(app);
-
+const db = getFirestore(
 // --- 1. GLOBALES ---
 let isPublicMode = false;
 let currentAlbergueId = null;

dc13fcc05dded28a342198f4eeabe178d8a37efb
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-05 09:32:56 +0100

    V.38.7.1. Ultima estable. V.38.4.0

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
 const firebaseConfig = { apiKey: "AIzaSyAzfEMwMd6M1VgvV0tJn7RS63RJghLE5UI", authDomain: "albergues-temporales.firebaseapp.com", projectId: "albergues-temporales", storageBucket: "albergues-temporales.firebasestorage.app", messagingSenderId: "489999184108", appId: "1:489999184108:web:32b9b580727f83158075c9" };
 const app = initializeApp(firebaseConfig); const auth = getAuth(app); const db = getFirestore(app);
 
-// --- 1. SISTEMA DE LOGS (ASIGNACIÓN DIRECTA A WINDOW) ---
+// --- 1. SISTEMA DE LOGS (ASIGNACIÓN DIRECTA A WINDOW)  ---
 window.sysLog = function(msg, type = 'info') {
     const c = document.getElementById('black-box-content');
     if (!c) { console.log(msg); return; }

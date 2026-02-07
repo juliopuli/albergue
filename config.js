1cc3a4e3bca071bbe0980685d9ce939868b14f68
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-07 17:16:47 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+// config.js
+export const firebaseConfig = {
+    apiKey: "TU_API_KEY_AQUI", 
+    authDomain: "albergues-temporales.firebaseapp.com",
+    projectId: "albergues-temporales",
+    storageBucket: "albergues-temporales.firebasestorage.app",
+    messagingSenderId: "489999184108",
+    appId: "1:489999184108:web:32b9b580727f83158075c9"
+};

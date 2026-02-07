10fe8b7e9130db32a474f723c08c3d5ef851f9b5
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-07 16:38:10 +0100

    V.1.5.2. Ultima estable. V.1.5.1

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+// Configuración de conexión a Firebase
+// Protegida por restricciones de dominio (Google Cloud Console)
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAzfEMwMd6M1VgvV0tJn7RS63RJghLE5UI", // Tu API Key
+    authDomain: "albergues-temporales.firebaseapp.com",
+    projectId: "albergues-temporales",
+    storageBucket: "albergues-temporales.firebasestorage.app",
+    messagingSenderId: "489999184108",
+    appId: "1:489999184108:web:32b9b580727f83158075c9"
+};

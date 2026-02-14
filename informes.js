f6ade9c9adecc9f1932eda5bc962967e09e54845
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-14 12:44:12 +0100

    V.4.0.1

--- informes.js
+++ informes.js
@@ -1,4 +1,4 @@
 function abrirInforme(tipo) {
     document.getElementById('zona-opciones-informe').innerHTML =
-      '<div style="margin-top:32px;text-align:center;color:#444;font-size:1.3em;">Has seleccionado: <b>' + tipo.charAt(0).toUpperCase() + tipo.slice(1) + '</b></div>';
+      '<b>Has seleccionado:</b> ' + tipo.charAt(0).toUpperCase() + tipo.slice(1);
 }

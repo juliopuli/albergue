2d7fe96069b5700867e11d8da2113e909b378839
juliopuli <49448979+juliopuli@users.noreply.github.com>
2026-02-14 12:37:11 +0100

    V.4.0.0
    Removed initial comment and DOMContentLoaded event listener. Added abrirInforme function to display selected report type.

--- informes.js
+++ informes.js
@@ -1,8 +1,4 @@
-// informes.js
-
-document.addEventListener("DOMContentLoaded", function() {
-    const mensaje = document.getElementById("mensaje-informe");
-    if (mensaje) {
-        mensaje.innerText = "¡El módulo de informes funciona correctamente!";
-    }
-});
+function abrirInforme(tipo) {
+    document.getElementById('zona-opciones-informe').innerHTML =
+      '<div style="margin-top:32px;text-align:center;color:#444;font-size:1.3em;">Has seleccionado: <b>' + tipo.charAt(0).toUpperCase() + tipo.slice(1) + '</b></div>';
+}
